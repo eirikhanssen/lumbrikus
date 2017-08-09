1350eac81b6307f76e645e72396b28707dfb1937
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-08-09 10:47:07 +0200

    comment code

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -12,36 +12,73 @@
 function lumbrikus_add_admin_page() {
 	
 	//Generate Lumbrikus Admin Page
-	add_menu_page( 'Lumbrikus Theme Options', 'Lumbrikus', 'manage_options', 'nafo_lumbrikus', 'lumbrikus_theme_admin_page', get_template_directory_uri() . '/img/l.icon.png', 110 );
+	add_menu_page(
+		'Lumbrikus Theme Options', // page title
+		'Lumbrikus', //menu title
+		'manage_options', // capability
+		'nafo_lumbrikus', // menu slug
+		'lumbrikus_theme_admin_page', // callable function
+		get_template_directory_uri() . '/img/l.icon.png', // icon url
+		110 // position
+		);
 	
 	//Generate Lumbrikus Admin Sub Pages
-	add_submenu_page( 'nafo_lumbrikus', 'Lumbrikus Theme Options', 'General', 'manage_options', 'nafo_lumbrikus', 'lumbrikus_theme_admin_page' );
-	add_submenu_page( 'nafo_lumbrikus', 'Lumbrikus CSS Options', 'Custom CSS', 'manage_options', 'nafo_lumbrikus_css', 'lumbrikus_theme_custom_css_page');
 	
-	
-	
+	add_submenu_page(
+		'nafo_lumbrikus', // str parent slug
+		'Lumbrikus Theme Options', // str page title
+		'General', // str menu title
+		'manage_options', // str capability
+		'nafo_lumbrikus', // str menu slug
+		'lumbrikus_theme_admin_page' // str callable function
+		);
+
+	add_submenu_page( 
+		'nafo_lumbrikus', // str parent slug
+		'Lumbrikus CSS Options', // str page title
+		'Custom CSS', // str menu title
+		'manage_options', // str capability
+		'nafo_lumbrikus_css', // str menu slug
+		'lumbrikus_theme_custom_css_page' // str callable function
+		);
 }
+
 add_action( 'admin_menu', 'lumbrikus_add_admin_page' );
 
 //Activate custom settings
-add_action( 'admin_init', 'lumbrikus_custom_settings' );
+
+add_action( 
+	'admin_init', // str tag
+	'lumbrikus_custom_settings' // callable function
+	);
 
 function lumbrikus_custom_settings() {
 	//  Add customizations here....
 
 	// Theme Support Options
 
-
 	// Custom CSS Options
 
-	register_setting( 'lumbrikus-custom-css-options', 'lumbrikus_css', 'lumbrikus_sanitize_custom_css' );
+	register_setting(
+		'lumbrikus-custom-css-options', // str option group
+		'lumbrikus_css', // str option name
+		'lumbrikus_sanitize_custom_css' // callback function (sanitizer)
+		);
 	
-	add_settings_section( 'lumbrikus-custom-css-section', 'Custom CSS', 'lumbrikus_custom_css_section_callback', 'nafo_lumbrikus_css' );
+	add_settings_section( 
+		'lumbrikus-custom-css-section', // str id
+		'Custom CSS', // str title
+		'lumbrikus_custom_css_section_callback', // callback function
+		'nafo_lumbrikus_css' // str page
+		);
 	
-	add_settings_field( 'custom-css', 'Insert your Custom CSS', 'lumbrikus_custom_css_callback', 'nafo_lumbrikus_css', 'lumbrikus-custom-css-section' );
-
-
-
+	add_settings_field( 
+		'custom-css', // str id
+		'Insert your Custom CSS', // str title
+		'lumbrikus_custom_css_callback', // callable callback function
+		'nafo_lumbrikus_css',  // str page
+		'lumbrikus-custom-css-section' // str section
+		);
 
 
 } // lumbrikus custom settings
