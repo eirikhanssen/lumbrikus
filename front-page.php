9a6e9bdce2d5975682f9ce859820beafb221ce27
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-10-12 15:05:45 +0200

    add front page template

---
+++ front-page.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+	<title>Lumbrikus</title>
+    <style type="text/css">
+        body {margin:0;}
+        .forside {
+            height: 100%;
+            width: 100%;
+            position: fixed;
+        }
+    </style>
+    <meta name="description" content="Naturfag på norsk, arabisk, somali og tigrinja. En læringsressurs med faglige og skjønnlitterære tekster og lydmateriale for småskoletrinn og nyankomne."/>
+</head>
+<body>
+    <object class="forside" type="image/svg+xml" data="https://lumbrikus.no/media/svg/lumbrikus.svg">
+        <h1>Lumbrikus.no</h1>
+        <nav>
+            <ul>
+                <li><a href="https://lumbrikus.no/kapitler/" title="Til kapitteloversikten som er inngangen til hovedinnholdet">Start lumbrikus.</a></li>
+                <li><a href="https://lumbrikus.no/til_laereren/" title="Lærerveiledningen inneholder bakgrunnsteori og forslag til hvordan man kan bruke lumbrikus.no i undervisningen.">Til lærerne</a></li>
+                <li><a href="https://lumbrikus.no/om-lumbrikus/" title="Bakgrunnsinformasjon om nettstedet, om lumbrikus-prosjektet og samarbeidspartnere.">Om Lumbrikus</a></li>
+                <li><a href="https://lumbrikus.no/til-foreldrene/" title="Veiledning og informasjon tilrettelagt foreldre av barn som bruker lumbrikus.no">Til foreldrene</a></li>
+            </ul>
+        </nav>
+    </object>
+</body>
+</html>
