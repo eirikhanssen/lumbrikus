79841b712c4f7c35346eaaae910bcbd35942711e
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-10-15 12:31:26 +0200

    use relative urls for front-page svg and links

--- front-page.php
+++ front-page.php
@@ -15,14 +15,14 @@
     <meta name="description" content="Naturfag på norsk, arabisk, somali og tigrinja. En læringsressurs med faglige og skjønnlitterære tekster og lydmateriale for småskoletrinn og nyankomne."/>
 </head>
 <body>
-    <object class="forside" type="image/svg+xml" data="https://lumbrikus.no/media/svg/lumbrikus.svg">
+    <object class="forside" type="image/svg+xml" data="<?php echo get_stylesheet_directory_uri();  ?>/media/svg/lumbrikus.svg">
         <h1>Lumbrikus.no</h1>
         <nav>
             <ul>
-                <li><a href="https://lumbrikus.no/kapitler/" title="Til kapitteloversikten som er inngangen til hovedinnholdet">Start lumbrikus.</a></li>
-                <li><a href="https://lumbrikus.no/til_laereren/" title="Lærerveiledningen inneholder bakgrunnsteori og forslag til hvordan man kan bruke lumbrikus.no i undervisningen.">Til lærerne</a></li>
-                <li><a href="https://lumbrikus.no/om-lumbrikus/" title="Bakgrunnsinformasjon om nettstedet, om lumbrikus-prosjektet og samarbeidspartnere.">Om Lumbrikus</a></li>
-                <li><a href="https://lumbrikus.no/til-foreldrene/" title="Veiledning og informasjon tilrettelagt foreldre av barn som bruker lumbrikus.no">Til foreldrene</a></li>
+                <li><a href="/kapitler/" title="Til kapitteloversikten som er inngangen til hovedinnholdet">Start lumbrikus.</a></li>
+                <li><a href="/til_laereren/" title="Lærerveiledningen inneholder bakgrunnsteori og forslag til hvordan man kan bruke lumbrikus.no i undervisningen.">Til lærerne</a></li>
+                <li><a href="/om-lumbrikus/" title="Bakgrunnsinformasjon om nettstedet, om lumbrikus-prosjektet og samarbeidspartnere.">Om Lumbrikus</a></li>
+                <li><a href="/til-foreldrene/" title="Veiledning og informasjon tilrettelagt foreldre av barn som bruker lumbrikus.no">Til foreldrene</a></li>
             </ul>
         </nav>
     </object>
