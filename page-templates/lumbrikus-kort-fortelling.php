c74cf894d0a47863858e8e5d7a2ef4c280c76029
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-10-24 22:49:51 +0200

    add a page template

---
+++ page-templates/lumbrikus-kort-fortelling.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+Template Name: Kort Fortelling
+Template Post Type: page
+*/
+// Page code here..
+//get_header( 'audiotext' );
+
+get_header();
+echo "<h1>Template: Kort Fortelling</h1>";
