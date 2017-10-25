6a9739e7d8d0a92a24f81134ea36d65f0992d6d1
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-10-25 17:55:05 +0200

    add function to create links to child pages. this function needs to be called in page templates or named templates

--- inc/custom-functions.php
+++ inc/custom-functions.php
@@ -19,10 +19,8 @@ return array(
 }
 
 
-function lumbrikus_has_children() {
-    global $post;
-
-    $children = get_pages( array( 'child_of' => $post->ID ) );
+function lumbrikus_has_children($post_id) {
+    $children = get_pages( array( 'child_of' => $post_id ) );
     if( count( $children ) == 0 ) {
         return false;
     } else {
@@ -33,12 +31,137 @@ function lumbrikus_has_children() {
 /**
  * find all pages that has page with $post_id as parent, and return a nav with links to them. If they have a fetured image, use that in the link.
  */
-function lumbrikus_make_nav_with_image_links_to_children_pages($post_id) {
-	$output ="<p>Post-id: <strong>" . $post_id ."</strong></p>";
-	$output .= "<nav class=\"childpagelinks\">\n";
-	$output .= "<ol>\n";
-	$output .= "<li>links to children will come here....</li>";
-	$output .= "</ol>\n";
-	$output .= "</nav>\n";
+function lumbrikus_make_text_links_to_children($post_id) {
+/*
+	$children[n]->post_title
+	$children[n]->post_name
+	$children[n]->ID
+	$children[n]->post_excerpt
+	$children[n]->post_parent
+	$children[n]->menu_order
+	$img = get_the_post_thumbnail($children[n])
+*/
+	$output = "\n<!-- BEGIN NAV TO CHILD PAGES -->\n";
+
+	if(lumbrikus_has_children($post_id)) {
+		$output .="<nav class=\"child-pages-text-links\">\n<ul>\n";
+		$all_children = get_pages( array( 'child_of' => $post_id ) );
+		$wanted_children = [];
+		
+		/* get all direct child pages */
+
+		foreach($all_children as $possible_child) {
+			if($possible_child->post_parent == $post_id) {
+				array_push($wanted_children, $possible_child);
+			}
+		}		
+
+		function compare_childpages($a, $b) {
+			return (int)$a->menu_order - (int)$b->menu_order;
+		}
+
+		usort($wanted_children, compare_childpages);
+
+		/* sort them depending on menu_order*/
+
+		foreach ($wanted_children as $childpage) {
+		
+			$link_text=$childpage->post_title;
+			$link_href=$childpage->post_name;
+
+			$li = "<li>";         
+			$li .= "<a href=\"" . $link_href . "\">";
+			$li .= $link_text;
+			$li .= "</a>";        
+			$li .= "</li>";
+			$output .= $li;
+		
+		}
+		/* $children loop end */
+
+		
+		$output .= "</ul>\n</nav>\n";
+	} else {
+		$output = "\n<!-- lumbrikus_make_text_links_to_children(): Denne siden har ingen barn. -->\n";
+	}
+
+	$output .= "\n<!-- END NAV TO CHILD PAGES -->\n";
+	
 	return $output;
 }
+
+
+/**
+ * find all pages that has page with $post_id as parent, and return a nav with links to them. If they have a fetured image, use that in the link.
+ */
+function lumbrikus_make_textimage_links_to_children($post_id) {
+/*
+	$children[n]->post_title
+	$children[n]->post_name
+	$children[n]->ID
+	$children[n]->post_excerpt
+	$children[n]->post_parent
+	$children[n]->menu_order
+	$img = get_the_post_thumbnail($children[n])
+*/
+	$output = "\n<!-- BEGIN NAV TO CHILD PAGES -->\n";
+
+	if(lumbrikus_has_children($post_id)) {
+		$output .="<nav class=\"child-pages-text-links\">\n<ul>\n";
+		$all_children = get_pages( array( 'child_of' => $post_id ) );
+		$wanted_children = [];
+		
+		/* get all direct child pages */
+
+		foreach($all_children as $possible_child) {
+			if($possible_child->post_parent == $post_id) {
+				array_push($wanted_children, $possible_child);
+			}
+		}		
+
+		function compare_childpages($a, $b) {
+			return (int)$a->menu_order - (int)$b->menu_order;
+		}
+
+		usort($wanted_children, compare_childpages);
+
+		/* sort them depending on menu_order*/
+
+		foreach ($wanted_children as $childpage) {
+		
+			$link_text=$childpage->post_title;
+			$link_href=$childpage->post_name;
+
+			$li = "<li>";         
+			$li .= "<a href=\"" . $link_href . "\">";
+			$li .= "<figure class=\"link-display\">";
+			if ( has_post_thumbnail($childpage) ) {
+    				$li .= get_the_post_thumbnail($childpage);
+				} else {
+					$li .= "<p>[Mangler bilde]</p>";
+				}
+			$li .= "<figcaption>" . $link_text . "</figcaption>";
+			$li .= "</figure>";
+			$li .= "</a>";        
+			$li .= "</li>";
+			$output .= $li;
+		
+		}
+		/* $children loop end */
+
+		
+		$output .= "</ul>\n</nav>\n";
+	} else {
+		$output = "\n<!-- lumbrikus_make_text_links_to_children(): Denne siden har ingen barn. -->\n";
+	}
+
+	$output .= "\n<!-- END NAV TO CHILD PAGES -->\n";
+	
+	return $output;
+}
+
+
+
+
+
+function lumbrikus_make_image_links_to_children($post_id) {}
