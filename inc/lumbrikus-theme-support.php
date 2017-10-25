46cca1521e7c35b1cbc44dd4115945c286cd88a7
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-10-25 17:54:25 +0200

    set size of post_thumbnail

--- inc/lumbrikus-theme-support.php
+++ inc/lumbrikus-theme-support.php
@@ -1,3 +1,8 @@
 <?php
 # when generating links to child-pages, it will be possible to use the featured image of the child page in the link.
-add_theme_support( 'post-thumbnails' ); 
+function lumbrikus_theme_support() {
+	add_theme_support( 'post-thumbnails' ); 
+	set_post_thumbnail_size( 200, 150 );	
+}
+
+add_action( 'after_setup_theme', 'lumbrikus_theme_support' );
