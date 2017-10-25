602c72851b4c5faa223650c8f1feebc628a150a5
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-10-25 19:30:19 +0200

    add gjøre og lage templates

--- page-templates/template-gjore-og-lage-underside.php
+++ page-templates/template-gjore-og-lage-underside.php
@@ -1,10 +1,34 @@
 <?php 
-/*
-Template Name: Gjøre og lage (underside)
-Template Post Type: page
-*/
-// Page code here..
-//get_header( 'audiotext' );
+/**
+ * Template Name: Gjøre og lage (underside)
+ * Template Post Type: page
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Lumbrikus
+ * @since 1.0
+ * @version 1.0
+ */
+get_header(); ?>
 
-get_header();
-echo "<h1>named template: Gjøre og lage (underside)</h1>";
+<!-- BEGIN NAMED PAGE GJØRE OG LAGE UNDERSIDE -->	
+<main class="site-main page-kapittel-hovedside" role="main">
+
+		<?php 
+			
+			if( have_posts() ):
+				
+				while( have_posts() ): the_post();
+
+					get_template_part( 'template-parts/content', 'gjore-og-lage-underside' );
+				
+				endwhile;
+				
+			endif;
+        
+		?>
+	
+</main>
+<!-- END NAMED PAGE GJØRE OG LAGE UNDERSIDE -->	
+<?php get_footer(); ?>
