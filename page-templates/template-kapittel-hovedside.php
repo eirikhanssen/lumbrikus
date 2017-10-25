9deba211a28a0e31836f25936bb8309f00681fd8
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-10-25 18:28:02 +0200

    add kapittel hovedside template

--- page-templates/template-kapittel-hovedside.php
+++ page-templates/template-kapittel-hovedside.php
@@ -1,10 +1,34 @@
 <?php 
-/*
-Template Name: Filmer (underside)
-Template Post Type: page
-*/
-// Page code here..
-//get_header( 'audiotext' );
+/**
+ * Template Name: Kapittel (hovedside)
+ * Template Post Type: page
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Lumbrikus
+ * @since 1.0
+ * @version 1.0
+ */
+get_header(); ?>
 
-get_header();
-echo "<h1>named template: Filmer (underside)</h1>";
+<!-- BEGIN NAMED PAGE KAPITTEL HOVEDSIDE -->	
+<main class="site-main page-kapittel-hovedside" role="main">
+
+		<?php 
+			
+			if( have_posts() ):
+				
+				while( have_posts() ): the_post();
+
+					get_template_part( 'template-parts/content', 'kapittel-hovedside' );
+				
+				endwhile;
+				
+			endif;
+        
+		?>
+	
+</main>
+<!-- END NAMED PAGE KAPITTEL HOVEDSIDE -->	
+<?php get_footer(); ?>
