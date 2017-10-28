67f184ebc100eabb2f204202654b0130c5f947cb
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-10-29 00:43:12 +0200

    add page-slug to body class

--- functions.php
+++ functions.php
@@ -4,3 +4,4 @@ require get_template_directory() . '/inc/function-admin.php';
 require get_template_directory() . '/inc/enqueue.php';
 require get_template_directory() . '/inc/custom-functions.php';
 require get_template_directory() . '/inc/lumbrikus-theme-support.php';
+require get_template_directory() . '/inc/lumbrikus-filters.php';
