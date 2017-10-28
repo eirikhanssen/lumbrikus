91c9ae446ff5b6b8603bb0fd0452d125daba2b98
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-10-29 00:44:27 +0200

    add ord template

---
+++ page-ord.php
@@ -0,0 +1,33 @@
+<?php 
+/**
+ * The template for displaying "Ord"
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Lumbrikus
+ * @since 1.0
+ * @version 1.0
+ */
+get_header(); ?>
+<?php echo lumbrikus_internal_chapter_menu(1); ?>
+<!-- BEGIN PAGE -->	
+<main class="site-main page-ord" role="main">
+
+		<?php 
+			
+			if( have_posts() ):
+				
+				while( have_posts() ): the_post();
+
+					get_template_part( 'template-parts/content', 'ord' );
+				
+				endwhile;
+				
+			endif;
+        
+		?>
+	
+</main>
+<!-- END PAGE -->	
+<?php get_footer(); ?>
