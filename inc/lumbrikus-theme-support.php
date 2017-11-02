34a383c9613f722e589b2bfb10f80034e517b412
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-11-02 23:51:05 +0100

    tweak image links, size of images

--- inc/lumbrikus-theme-support.php
+++ inc/lumbrikus-theme-support.php
@@ -2,7 +2,7 @@
 # when generating links to child-pages, it will be possible to use the featured image of the child page in the link.
 function lumbrikus_theme_support() {
 	add_theme_support( 'post-thumbnails' ); 
-	set_post_thumbnail_size( 200, 150 );	
+	set_post_thumbnail_size( 300, 225 );	
 }
 
 add_action( 'after_setup_theme', 'lumbrikus_theme_support' );
