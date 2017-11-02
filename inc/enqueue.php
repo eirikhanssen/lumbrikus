f34c1d50de62b371ef7c96d928e993546a64e66e
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-11-03 00:42:30 +0100

    tweak menu. show hints only for hovered/focused link. fade in hints

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -54,7 +54,7 @@ function lumbrikus_load_scripts() {
 	// wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.6', 'all' );
 	wp_enqueue_style( 'normalize', get_template_directory_uri() . '/css/normalize.css', array(), '7.0.0', 'all' );
 	wp_enqueue_style( 'googlefonts', 'https://fonts.googleapis.com/css?family=Open+Sans|Open+Sans+Condensed:300|Roboto', array(), '0.0.5', 'all' );
-	wp_enqueue_style( 'lumbrikus', get_template_directory_uri() . '/css/lumbrikus.css', array(), '1.0.7', 'all' );
+	wp_enqueue_style( 'lumbrikus', get_template_directory_uri() . '/css/lumbrikus.css', array(), '1.0.8', 'all' );
 	
 	/* register scripts */
 	lumbrikus_include_custom_jquery();
