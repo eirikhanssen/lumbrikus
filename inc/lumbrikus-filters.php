4dc67c207df883f1eeb7507ac9588bcd29dff378
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-11-07 15:21:49 +0100

    add some padding to main in foreldre læreren om, fix body slug, skip page- prefix

--- inc/lumbrikus-filters.php
+++ inc/lumbrikus-filters.php
@@ -4,7 +4,7 @@
 function lumbrikus_add_slug_body_class( $classes ) {
 	global $post;
 	if ( isset( $post ) ) {
-		$classes[] = $post->post_type . '-' . $post->post_name;
+		$classes[] = $post->post_name;
 	}
 	return $classes;
 }
