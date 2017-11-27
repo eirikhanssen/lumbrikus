e4cf38e200c9ceb3eb822f124f308f67c3d2aaf7
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-11-27 11:22:44 +0100

    revert

--- js/lumbrikus.js
+++ js/lumbrikus.js
@@ -1,11 +1,11 @@
 $( document ).ready(function() {
     console.log( "lumbrikus.js ready!" );
 
-    $('.menulink').click(function(e){
+    /*$('.menulink').click(function(e){
         e.preventDefault();
         var result = window.confirm("go to: " + $(e.target).attr("href") + "?");
         if(result) {
             window.location.href=$(e.target).attr("href");
         }
-    });
+    });*/
 });
