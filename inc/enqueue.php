54c5e1399ea5a98d01b82d4f7b5cc9b7a1365848
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-11-28 15:26:49 +0100

    update bg color for background image in text sync

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -54,7 +54,7 @@ function lumbrikus_load_scripts() {
 	// wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.6', 'all' );
 	wp_enqueue_style( 'normalize', get_template_directory_uri() . '/css/normalize.css', array(), '7.0.0', 'all' );
 	wp_enqueue_style( 'googlefonts', 'https://fonts.googleapis.com/css?family=Open+Sans|Open+Sans+Condensed:300|Roboto', array(), '0.0.5', 'all' );
-	wp_enqueue_style( 'lumbrikusCSS', get_template_directory_uri() . '/css/lumbrikus.css', array(), '1.2.4', 'all' );
+	wp_enqueue_style( 'lumbrikusCSS', get_template_directory_uri() . '/css/lumbrikus.css', array(), '1.2.5', 'all' );
 	
 	/* register scripts */
 	lumbrikus_include_custom_jquery();
@@ -72,7 +72,7 @@ function lumbrikus_load_scripts() {
 	if( is_page( 'kort' ) || is_page( 'lang' ) ) {
 		// enqueue js and css for text and audio playback/sync
 		// the order of enquing the scripts is significant - mejs needs to be enqueued before the other scripts
-		wp_enqueue_style( 'mediasyncCSS', '/css/mediasync.css', array(), '0.2.2', 'all' );
+		wp_enqueue_style( 'mediasyncCSS', '/css/mediasync.css', array(), '0.2.3', 'all' );
 		wp_enqueue_style( 'mejsCSS', '/lib/mediaelement/build/mediaelementplayer.css', array(), '4.2.6', 'all' );
 		wp_enqueue_script( 'mejs', '/lib/mediaelement/build/mediaelement-and-player.js', false, '4.2.6', true);
 		wp_enqueue_script( 'mediasyncJS', '/js/mediasync.js', false, '0.9.5', true);
