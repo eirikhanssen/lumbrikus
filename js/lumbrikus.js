49b4764c831ecbc7e73ce0f0f9613c6140ca721e
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-12-07 22:32:13 +0100

    tweak lightbox settings

--- js/lumbrikus.js
+++ js/lumbrikus.js
@@ -2,11 +2,10 @@ $( document ).ready(function() {
     console.log( "lumbrikus.js ready!" );
 
     lightbox.option({
-        'resizeDuration': 200,
         'wrapAround': true,
         'albumLabel': 'Bilde %1 av %2',
         'fadeDuration': 400,
-        'fadeImageDuration': 200,
-        'resizeDuration': 200
+        'fadeImageDuration': 50,
+        'resizeDuration': 100
       })
 });
