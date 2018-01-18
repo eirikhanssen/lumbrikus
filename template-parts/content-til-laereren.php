cc907f7a0c8e90431ff1e9bb7488ab90023519c5
Eirik Hanssen <eirikhanssen79@gmail.com>
2018-01-18 12:48:41 +0100

    switch to image menu for til læreren

---
+++ template-parts/content-til-laereren.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template part for displaying page content in page-til-laereren.php
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Lumbrikus
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<!-- BEGIN CONTENT-PAGE-TIL-LAEREREN -->
+<article id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+	<div class="entry-content">
+
+<!-- BEGIN THE CONTENT -->
+
+<?php the_content(); ?>
+
+<!-- END THE CONTENT -->		
+
+	</div><!-- .entry-content -->
+</article><!-- #page-## -->
+<?php echo lumbrikus_make_textimage_links_to_children(get_the_ID()); ?>
+
+<!-- END CONTENT-PAGE-TIL-LAEREREN -->
