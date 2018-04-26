4bb11d910e9fb2a6f54c9385cbd88d3409a4148e
Eirik Hanssen <eirikhanssen79@gmail.com>
2018-04-26 16:04:19 +0200

    rework the function to make links to lightbox galley for images

--- js/lumbrikus.js
+++ js/lumbrikus.js
@@ -57,15 +57,35 @@ $( document ).ready(function() {
       /* turn links to attachment pages into lightbox-links to display images in fullscreen */
 
       var turn_attachment_page_links_to_lightbox_showimage_links = (function(){
-        var attachments = $('.wp-caption a');
+        var attachments = $('.wp-caption');
         attachments.each(function(){
-            var url_attachmentpage=$(this).attr("href");
+            var url_attachmentpage="";
+            var attachment_page_link_html = "";
+            var has_attachment_page_url = false;
+            if($(this).find('a').length) {
+                console.log('yes');
+                url_attachmentpage=$(this).find('a').attr("href");
+                attachment_page_link_html = "<p class='additional-info'>Se <a href='" + url_attachmentpage + "'>vedleggssiden</a> for mer info om bildet.</p>";
+                has_attachment_page_url = true;
+            } else {
+                console.log('no');
+                $(this).addClass('no_attachment_link');
+            }
+            
             var url_smallimage=$(this).find('img').attr('src');
             var url_largeimage=url_smallimage.replace(/^(.+?)-\d+x\d+([.](jpg|jpeg|png|svg))$/,'$1$2');
-            var lbox_caption = '<p class="caption-text">' + $(this).parent().find('.wp-caption-text').html() + '</p>' + '<p class="additional-info">Se <a href="' + url_attachmentpage + '">vedleggssiden</a> for mer info om bildet.</p>';
-            $(this).attr('href', url_largeimage);
-            $(this).attr('data-lightbox', 'showimages');
-            $(this).attr('data-title', lbox_caption);
+            var lbox_caption = "<p class='caption-text'>" + $(this).find('.wp-caption-text').html() + '</p>' + attachment_page_link_html;
+            var caption_html_contents = $(this).html();
+            var lightbox_link_element_html = '<a href="'+ url_largeimage +'" data-lightbox="showimages" data-title="' + lbox_caption + '"></a>';
+            
+            if (has_attachment_page_url) {
+                $(this).find('a').attr('href', url_largeimage);
+                $(this).find('a').attr('data-lightbox', 'showimages');
+                $(this).find('a').attr('data-title', lbox_caption);
+            } else {
+                $(this).html(lightbox_link_element_html);
+                $(this).find('a').html(caption_html_contents);
+            }
         });
       })();
 });
