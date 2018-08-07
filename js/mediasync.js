edd5953b5cc67651b7bea3233a82e283f057eacc
Eirik Hanssen <eirikhanssen79@gmail.com>
2018-08-07 11:04:44 +0200

    add experimental support for interactive timed elements. they will set the audio time when clicked and start playing audio

--- js/mediasync.js
+++ js/mediasync.js
@@ -1,4 +1,4 @@
-function time2sec(timestring) {
+/*function time2sec(timestring) {
     // convert timestring to seconds
     'use strict';
     var timereg = new RegExp(/(\d+):(\d*[.]\d+)/);
@@ -9,7 +9,7 @@ function time2sec(timestring) {
     var sec_in_seconds = Number(timestring.replace(timereg, '$2'));
     var sec_total = sec_in_minutes + sec_in_seconds;
     return sec_total;
-}
+}*/
 
 function updateCueSync(player, lang) {
     //console.log('updateCuesync()');
@@ -31,7 +31,7 @@ function updateCueSync(player, lang) {
             }
         });
     }
-}
+} // updateCueSync(player, lang)
 
 function scrollTo(el) {
     //el.parent().scrollTop(el.offset().top - 30);
@@ -39,11 +39,11 @@ function scrollTo(el) {
     var childPos = el.offset()
     var parentPos = el.parent().offset()
     var childOffsetTop = childPos.top - parentPos.top
-
+    
     $('html, body').animate({
         scrollTop: childOffsetTop
-        }, scroll_duration);
-}
+    }, scroll_duration);
+} // scrollTo(el)
 
 var getCurrentEntryIndexFromTracks = function(player) {
     // the track with the same language as the audio will be of kind = captions
@@ -64,7 +64,7 @@ var getCurrentEntryIndexFromTracks = function(player) {
         }
     }
     return 0;
-}
+} // getCurrentEntryIndexFromTracks(player)
 
 function getCaptionCueFromPlayer(player, cueID) {
     var tracksObj = player.tracks;
@@ -76,7 +76,7 @@ function getCaptionCueFromPlayer(player, cueID) {
         }
     }
     return captionsTrack.entries[cueID].text;
-}
+} // getCaptionCueFromPlayer(player, cueID)
 
 var getCurrentSubtitleFromTracks = function (player, kind, srclang, entryIndex) {
     // tracksObj has several tracks
@@ -93,20 +93,20 @@ var getCurrentSubtitleFromTracks = function (player, kind, srclang, entryIndex) 
         }
     }
     return;
-}
+} // getCurrentSubtitleFromTracks(player, kind, srclang, entryIndex)
 
 var getTranslatedSubtitle = function(player, lang) {
     //return player.getCurrentTime() + ' : ' + getCurrentSubtitleFromTracks(player, 'subtitles', lang, getCurrentEntryIndexFromTracks(player));
     return getCurrentSubtitleFromTracks(player, 'subtitles', lang, getCurrentEntryIndexFromTracks(player));
-}
+} // getTranslatedSubtitle(player, lang)
 
 function getCurrentCueId(player, lang) {
-     var tracks = player.tracks;
-
-     var currentTime = player.getCurrentTime();
-     var i;
-     for (i = 0; i < tracks.length; i++) {
-         var currentTrack = tracks[i];
+    var tracks = player.tracks;
+    
+    var currentTime = player.getCurrentTime();
+    var i;
+    for (i = 0; i < tracks.length; i++) {
+        var currentTrack = tracks[i];
         if (currentTrack.kind == "captions" && currentTrack.srclang == lang) {
             var j;
             for(j=0; j<currentTrack.entries.length; j++) {
@@ -116,14 +116,14 @@ function getCurrentCueId(player, lang) {
                 }
             }
         }
-     }
-}
+    }
+} // getCurrentCueId(player, lang)
 
 function getPlayerFromLang(lang) {
     var playerId = $('#audio-' + lang).closest('.mejs__container').attr('id');
     var player = mejs.players[playerId];
     return player;
-}
+} // getPlayerFromLang(lang)
 
 function setTranslation (text) {
     if ($('#translation').html() != text) {
@@ -131,7 +131,7 @@ function setTranslation (text) {
     } if ($('body').attr('secondary_language') == 'img') {
         $('#translation').html('');
     }
-}
+} // setTranslation (text)
 
 function updateSecondaryLanguage(player) {
     var secondary_language = $('body').attr('data-secondary-lang');
@@ -141,7 +141,7 @@ function updateSecondaryLanguage(player) {
         translatedCueText = getTranslatedSubtitle(player, secondary_language);
     }
     setTranslation(translatedCueText);
-}
+} // updateSecondaryLanguage(player)
 
 $( document ).ready(function() {
     $('body').attr('data-current-key', 1);
@@ -150,7 +150,7 @@ $( document ).ready(function() {
         var player = getPlayerFromLang(lang);
         var mediaEl = $(this).find('audio')[0];
         var that = this;
-
+        
         function updateTextSync(){
             //console.log('updateTextSync()');
             currentPlayerKeys = $('article[data-lang='+lang+ '] .key');
@@ -168,15 +168,15 @@ $( document ).ready(function() {
                 }
                 $('body').attr('data-current-key', currentCueId);
             });
-
+            
             updateSecondaryLanguage(player);
         }
-
+        
         $(mediaEl).on("playing", function(){
             //console.log("playing: " + lang);
             updateTextSync();
             updateCueSync(player, lang);
-
+            
         });
         $(mediaEl).on("pause", function(){
             //console.log("paused: " + lang);
@@ -195,16 +195,16 @@ $( document ).ready(function() {
         });
         $(mediaEl).on("timeupdate", function(){
             //console.log("timeupdate of " + lang + ": " + player.getCurrentTime());
-
+            
             // visualize which key is currently being played
             // syncronize secondary_language text to primary_language text using keys (cue id)
             updateTextSync();
-
+            
             // visualize which sentence/part sentence is being played if there is a secondary layer of timings in span data-begin and data-end attributes.
             updateCueSync(player, lang);
         });
     });
-
+    
     function updateTranslationFromBodyAttrs() {
         // perform the actual update after 1 ms to allow the body attributes to be updated first
         window.setTimeout(function(){
@@ -218,13 +218,13 @@ $( document ).ready(function() {
             }
         },1,false);
     }
-
+    
     $('#secondary_language .labelradiobtn').on('click', function(){
         updateTranslationFromBodyAttrs();
     });
-
+    
     window.setTimeout(updateTranslationFromBodyAttrs, 100);
-});
+}); //$( document ).ready( function() {...} )
 
 
 
@@ -253,6 +253,69 @@ function addExtraButtons(){
     btns.appendChild(btn2);
     btns.setAttribute('style','position:fixed; bottom:0;right:0;display:flex;');
     document.querySelector('body').appendChild(btns);
-    }
+} // addExtraButtons()
 
 window.addEventListener('load', addExtraButtons,false);
+
+function time2sec(str) {
+    // convert timestring to seconds
+    var hours, minutes, seconds;
+    
+    function has_hours(str) {
+        if(str.match(/^\d+:\d+:\d+([.]\d+)?$/)) {
+            return true
+        }
+        return false;
+    }
+    
+    function has_minutes(str) {
+        if(str.match(/^(.+)?\d+:\d+([.]\d+)?$/)) {
+            return true
+        }
+        return false;
+    }
+    
+    function has_seconds(str){
+        if(str.match(/^(.+)?\d+([.]\d+)?$/)) {
+            return true
+        }
+        return false;
+    }
+    
+    if(has_hours(str)) {
+        hours =   parseInt(  str.replace(/^((\d+):)((\d+):)(\d+[.]\d+)$/, '$2'));
+        minutes = parseInt(  str.replace(/^((\d+):)((\d+):)(\d+[.]\d+)$/, '$4'));
+        seconds = parseFloat(str.replace(/^((\d+):)((\d+):)(\d+([.]\d+)?)$/, '$5'));
+    } else if(has_minutes(str)) {
+        hours = 0;
+        minutes = parseInt(  str.replace(/^((\d+):)?(\d+[.]\d+)$/, '$2'));
+        seconds = parseFloat(str.replace(/^((\d+):)?(\d+([.]\d+)?)$/, '$3'));
+    } else if(has_seconds(str)) {
+        hours = 0;
+        minutes = 0;
+        seconds = parseFloat(str);
+    } else {
+        return 0;
+    }
+    var result = seconds + 60*minutes + 3600*hours;
+    return result;
+} // time2sec(str)
+
+function add_timers_play_on_click_events() {
+	var audio_element = document.querySelector('.audio_and_text.no audio')
+	var timers = document.querySelectorAll('.audio_and_text.no span[data-begin][data-end]');
+	var timers_len = timers.length;
+    var step;
+
+	for(step = 0; step < timers_len; step++) {
+		timers[step].addEventListener('click', function(event){
+            var target = event.target;
+            var begin = target.getAttribute('data-begin');
+		    var end = target.getAttribute('data-end');
+            audio_element.setCurrentTime(time2sec(begin));
+            audio_element.play();
+		}, false);
+	}
+} // add_timers_play_on_click_events()
+
+window.addEventListener('load',add_timers_play_on_click_events, false);
